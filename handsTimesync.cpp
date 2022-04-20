f8791b1dbf9f142b6db6390feab735425f564e24
dodoradio <dodoradio@github.com>
2022-04-20 12:22:48 +0100

    Remove unneeded dependency and fix error handling Removed unneeded qsavefile dependency and caused an exit if the file cannot be opened

--- handsTimesync.cpp
+++ handsTimesync.cpp
@@ -1,7 +1,6 @@
 #include <QTime>
 #include <QDate>
 #include <QFile>
-#include <QSaveFile>
 #include <QTextStream>
 
 int main(int argc, char *argv[])
@@ -9,7 +8,8 @@ int main(int argc, char *argv[])
         QFile time_file("/sys/devices/sop716/time");
         if(!time_file.open(QIODevice::WriteOnly | QIODevice::Text))
         {
-            qDebug("Unable to open file for write. Check permissions");
+            qCritical("Unable to open file for write. Check permissions");
+            return(-1);
         }
         QTextStream out(&time_file);
         out << QDate::currentDate().toString(Qt::ISODate) << " " << QTime::currentTime().toString(Qt::TextDate);
