7fbaf751dc751524d69affb42b3759d3e8504e52
Arseniy Movshev <dodoradio@outlook.com>
2023-05-19 17:27:25 +0100

    Clean up inconsistent tab formatting
    Signed-off-by: Arseniy Movshev <dodoradio@outlook.com>

--- handsTimesync.cpp
+++ handsTimesync.cpp
@@ -5,15 +5,14 @@
 
 int main(int argc, char *argv[])
 {
-        QFile time_file("/sys/devices/sop716/time");
-        if(!time_file.open(QIODevice::WriteOnly | QIODevice::Text))
-        {
-            qCritical("Unable to open file for write. Check permissions");
-            return(-1);
-        }
-        QTextStream out(&time_file);
-        out << QDate::currentDate().toString(Qt::ISODate) << " " << QTime::currentTime().toString(Qt::TextDate);
-        time_file.close();
-        qDebug("Synchronised Narwhal hands with system time");
-
+    QFile time_file("/sys/devices/sop716/time");
+    if(!time_file.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        qCritical("Unable to open file for write. Check permissions");
+        return(-1);
+    }
+    QTextStream out(&time_file);
+    out << QDate::currentDate().toString(Qt::ISODate) << " " << QTime::currentTime().toString(Qt::TextDate);
+    time_file.close();
+    qDebug("Synchronised Narwhal hands with system time");
 }
